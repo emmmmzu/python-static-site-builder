6c9efcdb59e4ffadf92c74ece4e81fb3800bd2a7
emmmmzu <emma.reinbold@web.de>
2025-07-24 00:31:16 +0200

    maybs

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,4 +1,6 @@
 import os
+
+from pathlib import Path
 from markdown_blocks import markdown_to_html_node
 
 
@@ -32,15 +34,14 @@ def generate_page(from_path, template_path, dest_path, basepath):
 
 
 def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
-    entries = os.listdir(dir_path_content)
-    for entry in entries:
-        if not entry.endswith(".md") and not os.path.isdir(os.path.join(dir_path_content, entry)):
-            continue
-        elif os.path.isfile(os.path.join(dir_path_content, entry)):
-            html = entry.replace(".md", ".html")
-            generate_page(os.path.join(dir_path_content, entry), template_path, os.path.join(dest_dir_path, html), basepath)
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        if os.path.isfile(from_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, dest_path, basepath)
         else:
-            generate_pages_recursive(os.path.join(dir_path_content, entry), template_path, os.path.join(dest_dir_path, entry), basepath)
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
 
 
 def extract_title(md):
